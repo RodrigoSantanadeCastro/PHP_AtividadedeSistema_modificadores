ab3b7b7b8c147199e8ea9a4328797245aa4bebe7
Denipereira <164106702+Denipereira@users.noreply.github.com>
2024-09-12 14:31:34 -0300

    pg forms

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
-include_once ('inicio.php');
+require_once ('inicio.php');
+require_once ('Pessoa.php');
+require_once ('Funcionario.php');
+require_once ('Cliente');
+require_once ('Gerente.php');
+require_once ('Diretor.php');
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <h1>Cadastre-se</h1>
+    <form action="" method="post">
+        <pre>
 
+        </pre>
+    </form>
+</body>
+</html>
 
